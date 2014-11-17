b8bd3523028f1d6de8b9a91175d3bc21afb2cc71
MTD <mtdowd@gmail.com>
2014-11-17 15:15:06 -0500

    Adds High Voltage option within app; sets default root to High Voltage page

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ require "monban/constraints/signed_in"
 require "monban/constraints/signed_out"
 
 Rails.application.routes.draw do
-  constraints Monban::Constraints::SignedIn.new do
-    root "dashboards#show", as: :dashboard
-  end
-
-  constraints Monban::Constraints::SignedOut.new do
-    root "landings#show"
-  end
+  # constraints Monban::Constraints::SignedIn.new do
+  #   root "dashboards#show", as: :dashboard
+  # end
+  #
+  # constraints Monban::Constraints::SignedOut.new do
+  #   root "landings#show"
+  # end
 
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
