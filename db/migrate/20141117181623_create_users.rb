2e3a0fc232e609d5aab3a5ac00018473e62273d3
MTD <mtdowd@gmail.com>
2014-11-17 17:33:40 -0500

    Creating full doctor profile with name(first/last), address, and phone
    * Upating user_params for new user fields
    * Creating edit, update, and show controller actions
    * Creating edit and show pages for user
    * Modifying new user view form to include first and last name fields
    * Adding :edit, :update to user routes
    * Adjusting create_user migration

--- db/migrate/20141117181623_create_users.rb
+++ db/migrate/20141117181623_create_users.rb
@@ -1,9 +1,15 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
       t.string :email, null: false
       t.string :password_digest, null: false
+      t.string :street_address
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.string :phone
 
       t.timestamps null: false
     end
