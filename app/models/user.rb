6bcadeb58bd6ef4d7885645f8ab0e26be91d6c7f
MTD <mtdowd@gmail.com>
2014-11-20 16:25:21 -0500

    Moving default photo location for paperclip to public directory

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   has_attached_file :picture,
-    styles: { medium: "300x300>", thumb: "100x100>" },
-    default_url: "/images/:style/missing.png"
+    default_url: "/images/missing.png"
 
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
   validates :email, presence: true, uniqueness: true
